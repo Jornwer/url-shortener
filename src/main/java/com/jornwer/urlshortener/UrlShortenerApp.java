ffbef8cb2d8dd91d5bc1efaba3ad7241e9711718
Jornwer <57893366+Jornwer@users.noreply.github.com>
2020-08-25 13:02:28 +0300

    Added basic files

---
+++ src/main/java/com/jornwer/urlshortener/UrlShortenerApp.java
@@ -0,0 +1,11 @@
+package com.jornwer.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApp.class, args);
+    }
+}
